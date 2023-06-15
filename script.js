5bed486c55dd496218b02ef8bb36d8c8d0a3faec
CrazyNinjaBulbasaur <123968645+CrazyNinjaBulbasaur@users.noreply.github.com>
2023-06-15 08:46:15 +0200

    Update script.js with new apiRoot address

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://filip-tasks-remote.crazyninjabulba.repl.co/v1/tasks';
+  var apiRoot = 'https://fifip-remote-tasks.crazyninjabulba.repl.co/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
